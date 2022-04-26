5f6af012271ba64eca3d8fc5cdb5da38e3fe72ff
Slice913 <akeemayoade@gmail.com>
2022-04-26 14:45:50 -0500

    ADD: CartItems components, CartTotal component CHNG: move files to components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js';
+import Header from './components/Header';
+import CartItems from './components/CartItems';
+import CartTotal from './components/CartTotal';
 
 function App() {
 
   return (
     <div className="App">
         <Header title="Amazon Cart"/>
+        <div className="App-main">
+          <CartItems />
+          <CartTotal />
+        </div>
     </div>
   )
 }
