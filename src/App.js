b110b820ce4d5984207ef57d6f37226da8768713
Slice913 <akeemayoade@gmail.com>
2022-04-27 10:22:18 -0500

    ADD: CartItem component structure

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
         <Header title="Amazon Cart"/>
         <div className="App-main">
-          <CartItems />
+          <CartItems /> 
           <CartTotal />
         </div>
     </div>
